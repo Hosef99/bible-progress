e3232d625022aea2a456463436b39c5062e1a600
Hosef <joseph.txh@gmail.com>
2023-06-23 00:27:26 +0800

    fixed another bug

--- scripts/home.js
+++ scripts/home.js
@@ -109,6 +109,6 @@ function loadChapters(bookId) {
 }
 
 
-if (document.cookie != "") userRecord = JSON.parse(document.cookie);
+if (document.cookie != "") userRecord = JSON.parse(getCookie(document.cookie));
 loadBooks();
 
