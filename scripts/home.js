7b63550ece56611381bc8ee1305e7f79d8420109
Joseph Tan <joseph.txh@gmail.com>
2023-06-23 13:36:51 +0800

    Added progress bar and fixed many many bugs!

--- scripts/home.js
+++ scripts/home.js
@@ -4,6 +4,7 @@ data.chapters = JSON.parse(data.chapters)
 let userRecord = {}
 const books = document.querySelector("#Books");
 const chapters = document.querySelector("#Chapters");
+const motivatingWords = ["keep it up!", "you've got this!", "stay determined and focused!", "keep the fire burning!", "keep stretching towards the goal!"]
 
 function getCookie(cname) {
     let name = cname + "=";
@@ -21,6 +22,34 @@ function getCookie(cname) {
     return "";
 }
 
+function getReadChapters(bookId){
+    let totalRead = 0
+    let bookName = data.books[bookId-1].BookName;
+    if (userRecord[bookName] != null) {
+        for (let i = 0; i < data.books[bookId-1].TotalChapters; i++){
+            if (userRecord[bookName].verses[i].isRead){
+                totalRead++;
+            }
+        }
+        userRecord[bookName].readChapters = totalRead;
+        return totalRead;
+    }
+    else{
+        return 0
+    }
+}
+
+function getTotalRead(){
+    let totalRead = 0
+    for (let i = 0; i < data.books.length; i++){
+        let bookName = data.books[i].BookName;
+        if (userRecord[bookName] != null){
+            totalRead += userRecord[bookName].readChapters
+        }
+    }
+    return totalRead
+}
+
 function hideAll() {
     $("#Books").addClass("hidden");
     $("#Chapters").addClass("hidden");
@@ -28,6 +57,32 @@ function hideAll() {
 
 //This function only runs once
 function loadBooks() {
+    $("#Books").removeClass("hidden");
+    $("#Books").empty()
+    const progressBar = document.createElement("div")
+    const progressDescription = document.createElement("p")
+    let total = getTotalRead()
+    if (total < 600) {
+        progressDescription.textContent = "You have read " + total.toString() + " chapters out of 1189, " + motivatingWords[Math.floor(Math.random()*motivatingWords.length)]
+    }
+    else if (total < 900) {
+        progressDescription.textContent = "You have read " + total.toString() + " chapters out of 1189, you're almost there!"
+    }
+    else if (total < 1090) {
+        progressDescription.textContent = "You have read " + total.toString() + " chapters out of 1189, "+(1189-total).toString()+" more to go, you can do this!"
+    }
+    else if (total === 1189){
+        progressDescription.textContent = "You have finished the bible, you did it!"
+    }
+    progressBar.innerHTML = "<progress id='bible-progress' value='"+getTotalRead().toString()+"' max='1189'>"+"</progress>";
+    const seperator1 = document.createElement("hr")
+    const title1 = document.createElement("h2")
+    title1.textContent = "Old Testament"
+    books.appendChild(progressBar)
+    books.appendChild(progressDescription)
+    books.appendChild(seperator1)
+    books.appendChild(title1)
+
     for (let i = 0; i < data.books.length; i++) {
         if (i == 39) {
             const seperator = document.createElement("hr")
@@ -36,12 +91,19 @@ function loadBooks() {
             books.appendChild(seperator)
             books.appendChild(title)
         }
+        
         const element = data.books[i];
         const book = document.createElement("a");
         book.textContent = element.BookName;
-        book.href = "";
+        book.href = "javascript:void(0)";
         book.dataset.bookId = element.BookID;
         book.classList.add("book-link");
+        if(userRecord[data.books[i].BookName] != null){
+            if (userRecord[data.books[i].BookName].readChapters == parseInt(data.books[i].TotalChapters)) {
+                book.classList.add("ticked");
+            }
+        }
+        
         books.appendChild(book);
     }
 
@@ -58,16 +120,23 @@ function loadBooks() {
 
 function loadChapters(bookId) {
     let addNewRecord = true;
+    let readChapters = 0;
     if (userRecord[data.books[bookId - 1].BookName] != null) {
         addNewRecord = false;
+        readChapters = getReadChapters(bookId);
     }
     else {
-        userRecord[data.books[bookId - 1].BookName] = []
+        userRecord[data.books[bookId - 1].BookName] = {verses: [], readChapters: 0}
+        readChapters = 0;
     }
     $("#Chapters").removeClass("hidden").empty()
     const bookTitle = document.createElement("h2");
-    bookTitle.innerHTML = "<a href='#'>" + data.books[bookId - 1].BookName + "</a>";
+    const progressBar = document.createElement("div")
+    progressBar.classList.add("progress")
+    progressBar.innerHTML = "<label for='chapter-progress'>Chapter progress: </label><progress id='chapter-progress' value='"+readChapters.toString()+"' max='"+data.books[bookId-1].TotalChapters+"'>"+"</progress>";
+    bookTitle.innerHTML = "<a href='javascript:void(0)'>" + data.books[bookId - 1].BookName + "</a>";
     chapters.appendChild(bookTitle)
+    chapters.appendChild(progressBar)
 
     for (let i = 1; i <= data.books[bookId - 1].TotalChapters; i++) {
         const chapter = document.createElement("a");
@@ -79,13 +148,13 @@ function loadChapters(bookId) {
         chapter.classList.add("chapter-link");
         chapters.appendChild(chapter)
         if (addNewRecord) {
-            userRecord[data.books[bookId - 1].BookName].push({
+            userRecord[data.books[bookId - 1].BookName].verses.push({
                 chapter: i,
                 isRead: false
             })
         }
         else {
-            if (userRecord[data.books[bookId - 1].BookName][i - 1].isRead) {
+            if (userRecord[data.books[bookId - 1].BookName].verses[i - 1].isRead) {
                 chapter.classList.add("ticked")
             }
         }
@@ -95,16 +164,19 @@ function loadChapters(bookId) {
     $(".chapter-link").each(function () {
         $(this).on("click", function (event) {
             const chapterId = $(this).data("chapter-id");
-            userRecord[data.books[bookId - 1].BookName][chapterId - 1].isRead = !userRecord[data.books[bookId - 1].BookName][chapterId - 1].isRead;
+            userRecord[data.books[bookId - 1].BookName].verses[chapterId - 1].isRead = !userRecord[data.books[bookId - 1].BookName].verses[chapterId - 1].isRead;
             $(this).toggleClass("ticked");
+            $("#chapter-progress").attr("value", getReadChapters(bookId));
+            console.log(JSON.stringify(userRecord))
             document.cookie = "record=" + JSON.stringify(userRecord)
+            
             console.log(document.cookie)
         });
     });
 
     $("#Chapters h2").on("click", function (event) {
         hideAll()
-        $("#Books").removeClass("hidden");
+        loadBooks();
     })
 }
 
